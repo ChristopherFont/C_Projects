9dac20df7bb484271c7c53ba2672b0d1727c5e74
ChristopherFont <64507939+ChristopherFont@users.noreply.github.com>
2020-12-01 21:54:41 -0500

    Create maximumWealth.c

---
+++ maximumWealth.c
@@ -0,0 +1,15 @@
+// finds the max amount from a double pointer called accounts
+int maximumWealth(int** accounts, int accountsSize, int* accountsColSize) {
+    int max = 0;
+    
+    for (int i = 0; i < accountsSize; i++) {
+        int tmp = 0;
+        
+        for (int j = 0 ; j < *accountsColSize; j++)
+          tmp += accounts[i][j];
+        
+        if (tmp < max) max = tmp;
+    
+    }    
+    return max;
+}
